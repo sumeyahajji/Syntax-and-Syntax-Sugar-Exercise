666749f8604606bc33c763c4361a339ca3a297cb
sumeyahajji <abdinasirsumeya@gmail.com>
2023-09-11 13:55:14 +0300

    completed syntax task

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 8;
+            
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
         }
     }
 }
